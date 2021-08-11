b98a3f5ee6a6e37a8924134ade58d8c27c470c96
VCT <VCT136@Gmail.com>
2021-08-11 19:39:36 +0200

    animated skills list interaction

--- js/interaction.js
+++ js/interaction.js
@@ -19,32 +19,46 @@ function moveViewTo(targetElement) {
 
 function fullSkillsList() {
 
-    $("#skills").empty();
-    $("#skills").load("/elements.html #full-skills-list");
     moveViewTo("#skills");
+    setTimeout(() => {
+        $("#skills").addClass("slide-out");
+        $("#skills").on("animationend", () => {
+            $("#skills").empty();
+            $("#skills").load("/elements.html #full-skills-list");
+            $("#skills").removeClass("slide-out");
+            $("#skills").addClass("slide-in");
+        });
+    }, 500);
 }
 
 function specializeSkillsList() {
 
-    $("#skills").empty();
-    let url = window.location.href;
-    if (url.includes("coordinator")) {
-        $("#skills").load("/elements.html #team-coordinator-skills-list");
-    }
-    else if (url.includes("programmer")) {
-        $("#skills").load("/elements.html #programmer-skills-list");
-    }
-    else if (url.includes("gamedesign")) {
-        $("#skills").load("/elements.html #game-designer-skills-list");
-    }
-    else if (url.includes("visualdesign")) {
-        $("#skills").load("/elements.html #visual-designer-skills-list");
-    }
-    else {
-        $("#skills").load("/elements.html #full-skills-list", () => {
-            $("#skills button").css("display", "none");
-            $("#skills").append("<p>Oops! It appears that something went wrong trying to read this url, so I'll just leave the full skills list here for now.</p>");
-        });
-    }
     moveViewTo("#skills");
+    setTimeout(() => {
+        $("#skills").addClass("slide-out");
+        $("#skills").on("animationend", () => {
+            $("#skills").empty();
+            let url = window.location.href;
+            if (url.includes("coordinator")) {
+                $("#skills").load("/elements.html #team-coordinator-skills-list");
+            }
+            else if (url.includes("programmer")) {
+                $("#skills").load("/elements.html #programmer-skills-list");
+            }
+            else if (url.includes("gamedesign")) {
+                $("#skills").load("/elements.html #game-designer-skills-list");
+            }
+            else if (url.includes("visualdesign")) {
+                $("#skills").load("/elements.html #visual-designer-skills-list");
+            }
+            else {
+                $("#skills").load("/elements.html #full-skills-list", () => {
+                    $("#skills button").css("display", "none");
+                    $("#skills").append("<p>Oops! It appears that something went wrong trying to read this url, so I'll just leave the full skills list here for now.</p>");
+                });
+            }
+            $("#skills").removeClass("slide-out");
+            $("#skills").addClass("slide-in");
+        });
+    }, 500);
 }
